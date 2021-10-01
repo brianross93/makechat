8150e24e278cff985fc9f624adb3cf3e4203e6e1
brssnj45 <brssnj45@gmail.com>
2021-10-01 03:22:11 -0400

    users can join and leave chat

--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 //index.js
 $(document).ready(()=>{
-  const socket = io.connect();
-
-  //Keepz track of the current user
-  let currentUser;
+    const socket = io.connect();
+    let currentUser;
+    // Getz the online users from the server
+    socket.emit('get online users');
 
   $('#create-user-btn').click((e)=>{
     e.preventDefault();
@@ -40,6 +40,21 @@ socket.on('new message', (data) => {
         <p class="message-text">${data.message}</p>
       </div>
     `);
+    
   })
+  socket.on('get online users', (onlineUsers) => {
+    //You may have not have seen this for loop before. It's syntax is for(key in obj)
+    //Our usernames are keys in the object of onlineUsers.
+    for(username in onlineUsers){
+      $('.users-online').append(`<div class="user-online">${username}</div>`);
+    }
+  })
+  //Refresh the online user list
+    socket.on('user has left', (onlineUsers) => {
+    $('.users-online').empty();
+    for(username in onlineUsers){
+      $('.users-online').append(`<p>${username}</p>`);
+    }
+  });
 
 })
